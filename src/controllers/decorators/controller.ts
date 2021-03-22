757f236ae73e16b2174e2dbf25f5d23aa896e70f
Damilare <opeyemisamuel222@gmail.com>
2021-03-22 21:14:23 +0000

    define a route binder

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -10,6 +10,7 @@ export function controller(routePrefix: string) {
     for (const key in target.prototype) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata('path', target.prototype, key);
+      const method = Reflect.getMetadata('method', target.prototype, key)
 
       if (path) {
         router.get(`${routePrefix}${path}`, routeHandler);
